1b51d30ce7c7b9fbf1ff99a64e3352c5cf382a37
Степан Рудаков <19stepan94@mail.ru>
2024-06-28 22:46:32 +0500

    сделал третью задачу 4.6

--- Task01.js
+++ Task01.js
@@ -1,20 +1,20 @@
   'use strict'
 
-  // const allStudents = ['Иванов', 'Петров', 'Сидоров', 'Кузнецов', 'Смирнов', 'Попов', 'Соколов'];
+  const allStudents = ['Иванов', 'Петров', 'Сидоров', 'Кузнецов', 'Смирнов', 'Попов', 'Соколов'];
 
-  // const failedStudents = ['Сидоров', 'Смирнов', 'Попов'];
+  const failedStudents = ['Сидоров', 'Смирнов', 'Попов'];
 
-  // const filter = (allStudents , failedStudents) => {
+  const filter = (allStudents , failedStudents) => {
 
-  //   const newArr = [];
+    const newArr = [];
   
-  //   for (let i = 0; i < allStudents.length; i++) {
-  //       if (allStudents.includes('Сидоров', 'Смирнов', 'Попов')) {
-  //         newArr[i] = allStudents.filter(item => !failedStudents.includes(item));
-  //       }
-  //       return newArr;
-  //     }
-  //   }
+    for (let i = 0; i < allStudents.length; i++) {
+        if (allStudents.includes('Сидоров', 'Смирнов', 'Попов')) {
+          newArr[i] = allStudents.filter(item => !failedStudents.includes(item));
+        }
+        return newArr;
+      }
+    }
 
-  // const newArr = filter(allStudents , failedStudents);
-  // console.log('newArray:' , newArr);
+  const newArr = filter(allStudents , failedStudents);
+  console.log('newArray:' , newArr);
--- Task03.js
+++ Task03.js
@@ -1,1 +1,14 @@
  'use strict'
+
+ const names= ['Noah', 'Liam', 'Mason', 'Jacob', 'Robot', 'William', 'Ethan', 'Michael', 'Alexander'];
+
+ const addPrefix = () => {
+  let nameWithPrefix = 0;
+
+  for (let i = 0; i < names.length; i++) {
+    nameWithPrefix = names.map(i => 'Mr ' + i);
+  }
+  return nameWithPrefix;
+ }
+
+ console.log(addPrefix(names));
