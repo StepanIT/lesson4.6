ce10103a501ce1b7c1a2791f278f2b5cefd5978d
Степан Рудаков <19stepan94@mail.ru>
2024-06-28 22:11:58 +0500

    сделал вторую задачу 4.6

--- Task02.js
+++ Task02.js
@@ -1,1 +1,14 @@
  'use strict'
+
+ const allCashbox = [4500, 3210, 650, 1250, 7830, 990, 13900, 370];
+
+ const getAverageValue = () => {
+  let sum = 0;
+
+  for (let i = 0; i < allCashbox.length; i++) {
+    sum = sum + allCashbox[i];
+  }
+  return Math.round(sum / allCashbox.length);
+ }
+
+ console.log(getAverageValue(allCashbox));
